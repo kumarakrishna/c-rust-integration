689e6c7e217c7634b3094cf4b8c47b8fccd865e9
kumarakrishna <kumarakrishnavaleti@gmail.comm>
2023-11-22 18:50:03 +0530

    add updated utils.rs

--- code_orig/utils_rust/src/lib.rs
+++ code_orig/utils_rust/src/lib.rs
@@ -1,54 +1,86 @@
 use std::io::{self, Write};
-use std::os::unix::io::{FromRawFd, AsRawFd};
 use std::fs::File;
-use libc::FILE;
-use libc::fileno;
+use std::ffi::CStr;
+use std::os::raw::{c_char, c_void};
+use std::collections::VecDeque;
+use std::marker::PhantomData;
 
 #[no_mangle]
-pub extern "C" fn hexdump(fp: *mut FILE, data: *const u8, len: usize) -> io::Result<()> {
+pub extern "C" fn hexdump(fp: *mut c_void, data: *const u8, data_len: usize) -> i32 {
+    // Check for null pointers, if necessary
+    if fp.is_null() || data.is_null() {
+        return libc::EINVAL;
+    }
+
+    // Cast the fp back to the correct type
+    let writer_ptr: *mut Box<dyn Write> = fp as *mut Box<dyn Write>;
+
+    // Create a safe reference to the Write trait object from the raw pointer.
+    let _writer: &mut dyn Write = unsafe {
+        &mut *writer_ptr
+    };
+
     let mut offset = 0;
-    
-    // Cast the raw pointers to slices
-    let data_slice = unsafe { std::slice::from_raw_parts(data, len) };
+    let mut _index: u32;
 
-    // Convert the FILE pointer to a mutable Write reference
-    let mut file = unsafe { File::from_raw_fd(fileno(fp)) };
+    let mut file = File::create("./bin/output.txt").unwrap();
+    // let text_to_write = "Hello, this is some text written to a file.";
 
-    write!(&mut file, "+------+-------------------------------------------------+------------------+\n").unwrap();
+    if let Err(_) = file.write_all("+------+-------------------------------------------------+------------------+\n".as_bytes()) {
+        return libc::EFAULT; // Return an error code
+    }
 
-    while offset < len {
-        write!(&mut file, "| {:04x} | ", offset).unwrap();
-
-        for index in 0..16 {
-            if offset + index < len {
-                write!(&mut file, "{:02x} ", data_slice[offset + index]).unwrap();
-            } else {
-                write!(&mut file, "   ").unwrap();
-            }
+    while offset < data_len {
+        
+        let mut formatted_output = format!("| {:04x} | ", offset);
+        if let Err(_) = file.write_all(formatted_output.as_bytes()) {
+            return libc::EFAULT;
         }
 
-        write!(&mut file, "| ").unwrap();
-
         for index in 0..16 {
-            if offset + index < len {
-                if data_slice[offset + index].is_ascii() && data_slice[offset + index].is_ascii_graphic() {
-                    write!(&mut file, "{}", data_slice[offset + index] as char).unwrap();
-                } else {
-                    write!(&mut file, ".").unwrap();
+            if offset + index < data_len {
+                formatted_output = format!("{:02x} ", unsafe { *data.add((offset + index) as usize) });
+                if let Err(_) = file.write_all(formatted_output.as_bytes()) {
+                    return libc::EFAULT;
                 }
-            } else {
-                write!(&mut file, " ").unwrap();
+            } else if let Err(_) = file.write_all("   ".as_bytes()) {
+                return libc::EFAULT;
             }
         }
 
-        write!(&mut file, " |\n").unwrap();
+        if let Err(_) = file.write_all("| ".as_bytes()) {
+            return libc::EFAULT;
+        }
+
+        for index in 0..16 {
+            if offset + index < data_len {
+                if let Some(byte) = unsafe { data.add((offset + index) as usize).as_ref() } {
+                    if byte.is_ascii() && byte.is_ascii_graphic() {
+                        formatted_output = format!("{} ", *byte as char);
+                        if let Err(_) = file.write_all(formatted_output.as_bytes()) {
+                            return libc::EFAULT;
+                        }
+                    } else if let Err(_) = file.write_all(".".as_bytes()) {
+                        return libc::EFAULT;
+                    }
+                }
+            } else if let Err(_) = file.write_all(" ".as_bytes()) {
+                return libc::EFAULT;
+            }
+        }
+
+        if let Err(_) = file.write_all(" |\n".as_bytes()) {
+            return libc::EFAULT;
+        }
 
         offset += 16;
     }
 
-    write!(&mut file, "+------+-------------------------------------------------+------------------+\n").unwrap();
+    if let Err(_) = file.write_all("+------+-------------------------------------------------+------------------+\n".as_bytes()) {
+        return libc::EFAULT;
+    }
 
-    Ok(())
+    0
 }
 
 const BIG_ENDIAN: i32 = 4321;
@@ -129,3 +161,81 @@ pub extern "C" fn cksum16(addr: *const u16, count: usize, init: u32) -> u16 {
     }
 }
 
+#[no_mangle]
+pub extern "C" fn write_to_stderr(message: *const c_char) -> u32 {
+    // Safety: Ensure that the message pointer is valid and points to a null-terminated C string.
+    let message_cstr = unsafe { CStr::from_ptr(message) };
+
+    if let Ok(message_str) = message_cstr.to_str() {
+        let stderr = io::stderr();
+        let mut handle = stderr.lock();
+
+        if let Err(e) = writeln!(&mut handle, "{}", message_str) {
+            eprintln!("Error writing to stderr: {}", e);
+            return 1; // Return a non-zero value to indicate an error.
+        }
+        return 0; // Return 0 to indicate success.
+    } else {
+        eprintln!("Error converting message to a valid string.");
+        return 1; // Return a non-zero value to indicate an error.
+    }
+}
+
+
+#[allow(unused_attributes)]
+#[allow(dead_code)]
+#[no_mangle]
+struct QueueEntry<T> {
+    next: Option<Box<QueueEntry<T>>>,
+    data: T,
+    _marker: PhantomData<T>,
+}
+
+#[allow(unused_attributes)]
+#[allow(dead_code)]
+struct Queue<T> {
+    queue: VecDeque<QueueEntry<T>>,
+    _marker: PhantomData<T>,
+}
+ 
+#[allow(dead_code)]
+impl<T> QueueEntry<T> {
+    fn new(data: T) -> Self {
+        QueueEntry {
+            next: None,
+            data,
+            _marker: PhantomData,
+        }
+    }
+}
+
+#[allow(dead_code)]
+impl<T> Queue<T> {
+    fn new() -> Self {
+        Queue {
+            queue: VecDeque::new(),
+            _marker: PhantomData,
+        }
+    }
+ 
+    fn push(&mut self, data: QueueEntry<T>) {
+        self.queue.push_back(data);
+    }
+ 
+    fn pop(&mut self) -> Option<QueueEntry<T>> {
+        self.queue.pop_front()
+    }
+ 
+    fn peek(&self) -> Option<&T> {
+        self.queue.front().map(|entry| &entry.data)
+    }
+ 
+    fn foreach<F>(&mut self, mut func: F)
+    where
+        F: FnMut(&mut T),
+    {
+        for entry in &mut self.queue {
+            func(&mut entry.data);
+        }
+    }
+}
